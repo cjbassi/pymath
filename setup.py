c64e422628088195e3284758c48e2ecc98b03518
Caleb Bassi <calebjbassi@gmail.com>
2018-10-07 16:40:11 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pymath2',
-    version='0.3.0',
+    version='1.0.0',
     packages=find_packages(exclude=('tests',)),
     scripts=['scripts/pymath'],
     description='Easy calculations on the command line with Python',
