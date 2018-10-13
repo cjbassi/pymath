8a5ba8955415dc10799c5801f7b8f422a0e6a714
Caleb Bassi <calebjbassi@gmail.com>
2018-10-12 19:53:57 -0700

    Update setup.py
    - Add fields
    - Fix bug with tests folder being included in installation

--- setup.py
+++ setup.py
@@ -5,8 +5,11 @@ from setuptools import find_packages, setup
 setup(
     name='pymath2',
     version='2.0.0',
-    packages=find_packages(exclude=('tests',)),
-    scripts=['scripts/pymath'],
     description='Easy calculations on the command line with Python',
+    author='Caleb Bassi',
     url='https://github.com/cjbassi/pymath',
+    packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*', 'tests']),
+    include_package_data=True,
+    license='MIT',
+    scripts=['scripts/pymath'],
 )
