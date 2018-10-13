7d31a897b9a805dc7470a564f85114e519dc5b7d
Caleb Bassi <calebjbassi@gmail.com>
2018-10-12 19:55:15 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pymath2',
-    version='2.0.0',
+    version='2.0.1',
     description='Easy calculations on the command line with Python',
     author='Caleb Bassi',
     url='https://github.com/cjbassi/pymath',
