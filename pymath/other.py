854220e1bee35b3c9b996d5e43eacd339628a1ca
Caleb Bassi <calebjbassi@gmail.com>
2018-10-26 19:50:19 -0700

    Change function name to convert_to_base_10

--- pymath/other.py
+++ pymath/other.py
@@ -1,10 +1,10 @@
 import string
 from typing import Dict, List, Tuple
 
-__all__ = ['benfords_law', 'convert_base', 'convert_base_10', 'rank']
+__all__ = ['benfords_law', 'convert_base', 'convert_to_base_10', 'rank']
 
 
-def convert_base_10(x: int, base: int) -> str:
+def convert_to_base_10(x: int, base: int) -> str:
     """https://stackoverflow.com/questions/2267362/how-to-convert-an-integer-in-any-base-to-a-string"""
     digits = string.digits + string.ascii_uppercase
     if x < 0:
@@ -30,8 +30,8 @@ def convert_base(x: str, source: int, dest: int) -> str:
         'n' converted from base 'source' to base 'dest'
     """
     if source == 10:
-        return convert_base_10(int(x), dest)
-    return convert_base_10(int(x, source), dest)
+        return convert_to_base_10(int(x), dest)
+    return convert_to_base_10(int(x, source), dest)
 
 
 def benfords_law(_list: List[float], first_digit=True) -> \
